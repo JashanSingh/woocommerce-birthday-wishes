7f4e576d4222ba95055adcf291190b8a1f0a92da
Jashan Singh <contact@jashansingh.com>
2023-03-13 07:51:23 +0100

    Implement birthday wishes function

--- birthday-wishes.php
+++ birthday-wishes.php
@@ -21,3 +21,24 @@ function enqueue_stylesheet()
 {
     wp_enqueue_style('birthday-wishes-stylesheet', plugins_url('/assets/css/style.css', __FILE__), false, '1.0.0', 'all');
 }
+
+/**
+ * Handle the birthday wishes in the WooCommerce user account dashboard.
+ * 
+ * This function is hooked to specific WooCommerce hook. In case, WooCommerce is not installed this can't be executed.
+ */
+add_filter('woocommerce_account_dashboard', 'add_birthday_wishes_text');
+
+function add_birthday_wishes_text()
+{
+    // Get all the users meta in one shoot.
+    $usermeta = get_user_meta(get_current_user_id());
+
+    // Instantiate the user class.
+    $user = new BirthdayWishes_User($usermeta);
+
+    // Print the birthday wishes.
+    if ($user->IsBirthday()) {
+        echo '<div class="birthday-wishes-box">Auguri <strong>'. $user->getFullName() .'</strong> per i tuoi <strong>'. $user->getAge() .'</strong> anni!</div>';
+    }
+}

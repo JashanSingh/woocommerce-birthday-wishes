bc2b890e06a06f4d7cebb1f9d82b804345391374
Jashan Singh <contact@jashansingh.com>
2023-03-13 07:47:29 +0100

    Create plugin skeleton

---
+++ birthday-wishes.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Birthday Wishes
+Description: This plugin adds to the user account dashboard the birthday greetings if it is the user's birthday. Required Woocommerce.
+Version: 1.0
+Author: Jashan Preet Singh
+Author URI: https://jashansingh.com
+*/

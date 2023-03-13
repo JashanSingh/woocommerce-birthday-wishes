3c3fe44cf031847ba6047b3f9c832459b7956fe4
Jashan Singh <contact@jashansingh.com>
2023-03-13 07:49:44 +0100

    Import plugin stylesheet

--- birthday-wishes.php
+++ birthday-wishes.php
@@ -11,3 +11,13 @@ Author URI: https://jashansingh.com
  * Load the plugin classes.
  */
 require_once __DIR__ . '/classes/BirthdayWishes_User.php';
+
+/**
+ * Load the plugin stylesheet.
+ */
+add_action('wp_enqueue_scripts', 'enqueue_stylesheet');
+
+function enqueue_stylesheet()
+{
+    wp_enqueue_style('birthday-wishes-stylesheet', plugins_url('/assets/css/style.css', __FILE__), false, '1.0.0', 'all');
+}
